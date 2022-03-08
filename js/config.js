9391a26dc44725882a56a81cc3df935ed85cb25a
José Lastra <53153650+lastrafda@users.noreply.github.com>
2022-03-08 16:08:11 -0300

    set configuration

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "eu-central-1_PyPHAve4y", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "82pmbvo6flf5agm2b31iofeah", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "eu-central-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
